6957f0734f075603fbf6a71805378927ddaeccce
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-06 10:07:07 +0200

    i create test class

---
+++ test_user.py
@@ -0,0 +1,8 @@
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test case of user features
+    Args:
+        unittest.TestCase: class helps to create the test of class"
+    '''
