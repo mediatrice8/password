be1f02a7464608b51586299757732282b503a026
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-06 11:32:47 +0200

    i add the tearDown method

--- test_user.py
+++ test_user.py
@@ -12,10 +12,17 @@ class TestUser(unittest.TestCase):
         set up method torun before each test case.
         '''
         self.new_user = User("media","0000")
+    def tearDown(self):
+        '''
+        tearDown method to clean up after test runs
+        '''
+         User.user_names=[]
+#------------------first test-------------------
     def  test_init(self):
         '''
         test_init used to test if the object is initialized well
         '''
         self.assertEqual(self.new_user.user_name,"media")
         self.assertEqual(self.new_user.password,"0000")
+# -----------------second test---------------------
         
