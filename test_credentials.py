08b3c5d5b6bf69429a8ed0ebb58d40039744c190
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-06 15:41:25 +0200

    update

--- test_credentials.py
+++ test_credentials.py
@@ -29,18 +29,18 @@ class TestCredential(unittest.TestCase):
         self.assertEqual(self.new_credential.password,"5555")
 #-------------------second test----------------------
     def test_save_credential(self):
-        """
+        '''
         test save credential test case to test if the contact object is saved into the credential_list
-        """
+        '''
         self.new_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),1)
 # ---------------third test---------------------------
     def test_save_multiple_credentials(self):
-        """
+        '''
         test_save_multiple_credentials to check if it can save multiple credentials objects to our credential_list
-        """
+        '''
         self.new_credential.save_credential()
         test_credential=Credential("Facebook","eussy","457823")
         test_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),2)
-
+# ---------------------------four test------------------
